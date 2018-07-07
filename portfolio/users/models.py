580c23701de4621e5f2eb1999837a4f859cf6e44
mccarrion <matthewccarrion@gmail.com>
2018-07-06 21:28:19 -0500

    Transferred over code from users app from cookiecutter django

--- portfolio/users/models.py
+++ portfolio/users/models.py
@@ -1,3 +1,20 @@
+import uuid
+
+from django.contrib.auth.models import AbstractUser
+from django.core.urlresolvers import reverse
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    """
+    This is an extendable model that will represent users across the website.
+    """
+    name = models.CharField(blank=True, max_length=255)
+    unique_id = models.UUIDField(default=uuid.uuid4, editable=False,
+                                 unique=True)
+
+    def __str__(self):
+        return self.username
+
+    def get_absolute_url(self):
+        return reverse('users:detail', kwargs={'username': self.username})
--- portfolio/users/views.py
+++ portfolio/users/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.urlresolvers import reverse
+from django.views.generic import DetailView, ListView, RedirectView, UpdateView
 
-# Create your views here.
+from .models import User
+
+class UserDetailView(LoginRequiredMixin, DetailView):
+    model = User
+    slug_field = 'username'
+    slug_url_kwarg = 'username'
+
+
+class UserRedirectView(LoginRequiredMixin, RedirectView):
+    permanent = False
+
+    def get_redirect_url(self):
+        return reverse('users:detail',
+                       kwargs={'username': self.request.user.username})
+
+
+class UserUpdateView(LoginRequiredMixin, UpdateView):
+    fields = ['name', ]
+    model = User
+
+    def get_success_url(self):
+        return reverse('users:detail',
+                       kwargs={'username': self.request.user.username})
+
+    def get_object(self):
+        return User.objects.get(username=self.request.user.username)
+
+
+class UserListView(LoginRequiredMixin, ListView):
+    model = User
+    slug_field = 'username'
+    slug_url_kwarg = 'username'
