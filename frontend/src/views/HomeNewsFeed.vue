e55731da10eb533b895bea33a2dec9ae50730b40
mccarrion <matthewccarrion@gmail.com>
2018-09-29 15:20:33 -0700

    Starting to add first vuejs views following example project

---
+++ frontend/src/views/HomeNewsFeed.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home-newsfeed">
+    <RwvArticleList type="all" />
+  </div>
+</template>
+<script>
+  import RwvArticleList from '@/components/ArticleList'
+
+  export default {
+    name: 'rwv-home-newsfeed',
+    components: {
+      RwvArticleList
+    }
+  }
+</script>
