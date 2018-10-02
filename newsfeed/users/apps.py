5f0f22dfb6508483fac04925b7302d0abcb1c04c
mccarrion <matthewccarrion@gmail.com>
2018-10-02 11:52:56 -0700

    Working towards getting tests to run

--- newsfeed/users/apps.py
+++ newsfeed/users/apps.py
@@ -3,3 +3,10 @@ from django.apps import AppConfig
 
 class UsersConfig(AppConfig):
     name = 'newsfeed.users'
+    verbose_name = "Users"
+
+    def ready(self):
+        try:
+            import users.signals  # noqa F401
+        except ImportError:
+            pass
