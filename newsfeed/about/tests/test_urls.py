7be47543f051bfe262c2e2eee3c256b97ac928d1
mccarrion <matthewccarrion@gmail.com>
2018-10-06 11:37:13 -0700

    Debugging url pattern tests

--- newsfeed/about/tests/test_urls.py
+++ newsfeed/about/tests/test_urls.py
@@ -1,17 +1,18 @@
 from django.urls import include, path, reverse
+from rest_framework import status
 from rest_framework.test import APITestCase, URLPatternsTestCase
 
 
 class ArticleUrlTests(APITestCase, URLPatternsTestCase):
     urlpatterns = [
-        path('api/', include('api.urls')),
+        path('api/', include('newsfeed.about.urls')),
     ]
 
-    def test_create_account(self):
+    def test_url_pattern(self):
         """
-        Ensure we can create new object.
+        Ensure that the views exist at the url pattern provided.
         """
-        url = reverse('')
+        url = reverse('about')
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
--- newsfeed/users/tests/test_urls.py
+++ newsfeed/users/tests/test_urls.py
@@ -1,17 +1,18 @@
 from django.urls import include, path, reverse
+from rest_framework import status
 from rest_framework.test import APITestCase, URLPatternsTestCase
 
 
 class ArticleUrlTests(APITestCase, URLPatternsTestCase):
     urlpatterns = [
-        path('api/', include('api.urls')),
+        path('api/', include('newsfeed.users.urls')),
     ]
 
     def test_create_account(self):
         """
         Ensure we can create new object.
         """
-        url = reverse('')
+        url = reverse('users')
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
