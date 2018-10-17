0cd85e535c246d0b60cee5da714cc492a653e26d
mccarrion <matthewccarrion@gmail.com>
2018-10-16 18:23:08 -0700

    Added article list with TODO, need to refactor later

---
+++ frontend/src/components/ArticleList.js
@@ -0,0 +1,41 @@
+// TODO: Will need to create a way to fetch articles based on the subject that was
+// selected for the article.
+
+import ArticlePreview from './ArticlePreview';
+import ListPagination from './ListPagination';
+import React from 'react';
+
+const ArticleList = props => {
+  if (!props.articles) {
+    return (
+      <div className="article-preview">Loading...</div>
+    );
+  }
+
+  if (props.articles.length === 0) {
+    return (
+      <div className="article-preview">
+        No articles are here... yet.
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      {
+        props.articles.map(article => {
+          return (
+            <ArticlePreview article={article} key={article.slug} />
+          );
+        })
+      }
+
+      <ListPagination
+        pager={props.pager}
+        articlesCount={props.articlesCount}
+        currentPage={props.currentPage} />
+    </div>
+  );
+};
+
+export default ArticleList;
