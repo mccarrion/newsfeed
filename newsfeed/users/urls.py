5e81153fb8e872467c4b7715864883611c0d2540
mccarrion <matthewccarrion@gmail.com>
2018-10-22 14:23:58 -0700

    Redoing urls for user app

--- newsfeed/users/urls.py
+++ newsfeed/users/urls.py
@@ -1,12 +1,22 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import UserViewSet
+from .views import (LoginView, LogoutView, PasswordChangeView,
+    PasswordResetConfirmView, PasswordResetView, UserDetailsView)
 
-router = DefaultRouter()
-router.register(r'users', UserViewSet)
 
 app_name = "users"
 urlpatterns = [
-    path(r'', include(router.urls)),
+    # URLs without a token needed
+    path(r'password/reset/', PasswordResetView.as_view(),
+         name='rest_password_reset'),
+    path(r'password/reset/confirm/', PasswordResetConfirmView.as_view(),
+         name='rest_password_reset_confirm'),
+    path(r'login/', LoginView.as_view(), name='rest_login'),
+
+    # URLs that need a logged in user with a valid token
+    path(r'logout/', LogoutView.as_view(), name='rest_logout'),
+    path(r'user/', UserDetailsView.as_view(), name='rest_user_details'),
+    path(r'password/change/', PasswordChangeView.as_view(),
+         name='rest_password_change'),
 ]
