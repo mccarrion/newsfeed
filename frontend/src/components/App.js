f21db9623d243a759c3ea7a8971821735b455cf4
mccarrion <matthewccarrion@gmail.com>
2018-10-25 15:36:24 -0700

    Adding initial route paths for app.js

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,24 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Route exact path="/" component={Home} />
+          <Route path="/login"  component={Login} />
+          <Route path="/signup" component={SignUp} />
+          <Router exact path="/@:username" component={UserProfile} />
+          <Router path="/@:username/comments" component={UserComments} />
+          <Router path="/@:username/favorites" component={UserFavorites} />
+          <Route exact path="/:subject" component={Subject} />
+          <Route path="/:subject/:title" component={Article} />
         </header>
       </div>
     );
