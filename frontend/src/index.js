265e728bb86c757657493b995968bc306b0daa79
mccarrion <matthewccarrion@gmail.com>
2018-10-25 10:29:23 -0700

    Moved App into components, setting up router for index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { App } from './components';
 import * as serviceWorker from './serviceWorker';
+import createHistory from 'history/createBrowserHistory';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export const history = createHistory();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDom.render(
+  <Router history={history}>
+    <Switch>
+      <Route path="/" component={App} />
+    </Switch>
+  </Router>
+), document.getElementById('root');
