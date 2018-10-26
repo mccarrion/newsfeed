6f81dd62dc1e240607052f9f57268977962f1126
mccarrion <matthewccarrion@gmail.com>
2018-10-26 15:58:22 -0700

    Starting to add login and signup components

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,14 @@
+
+
+class Login extends React.Component{
+
+  render() {
+    return (
+      <div className="Login">
+        <form onSubmit={this.handleSubmit}>
+          
+        </form>
+      </div>
+    )
+  }
+}
---
+++ frontend/src/components/SignUp.js
@@ -0,0 +1,14 @@
+
+
+class SignUp extends React.Component {
+
+  render() {
+    return (
+      <div className="SignUp">
+        <form onSubmit={this.handleSubmit}>
+          
+        </form>
+      </div>
+    )
+  }
+}
