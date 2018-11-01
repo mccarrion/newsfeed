ad3bdd0cd17475d35896c06ffb4a13642e57253a
mccarrion <matthewccarrion@gmail.com>
2018-11-01 11:00:05 -0700

    Adding a little more to login page

--- frontend/src/components/User/Login.js
+++ frontend/src/components/User/Login.js
@@ -1,12 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 class Login extends React.Component{
+  constructor(props) {
+    super(props);
+  }
 
   render() {
     return (
       <div className="Login">
         <form onSubmit={this.handleSubmit}>
-          
+
         </form>
       </div>
     )
