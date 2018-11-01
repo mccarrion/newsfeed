cd94b9cd357406d9774e371c2701f38e5c48b376
mccarrion <matthewccarrion@gmail.com>
2018-10-31 17:23:35 -0700

    Adding links to header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Header extends React.Component {
   render() {
     return (
+      <nav className="navbar">
+        <div>
 
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              NewsFeed
+            </Link>
+          </li>
+
+          <li className="nav-item">
+            <Link to="/about" className="nav-link">
+              About
+            </Link>
+          </li>
+
+          {/* Need to have for loops to create links. */}
+          <li className="nav-item">
+            <Link to="/:subject" className="nav-link">
+              Subject
+            </Link>
+          </li>
+        </div>
+      </nav>
     );
   }
 }
