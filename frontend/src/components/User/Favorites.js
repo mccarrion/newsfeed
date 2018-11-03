c62d227e377c9e0209a7b342a2315810f7a19d2c
mccarrion <matthewccarrion@gmail.com>
2018-11-03 12:20:00 -0700

    Adding Favorite articles component

---
+++ frontend/src/components/User/Favorites.js
@@ -0,0 +1,11 @@
+import { Profile } from './Profile';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class Favorites extends Profile {
+  render() {
+    return (
+
+    );
+  }
+}
