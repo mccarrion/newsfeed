fed6c1a33f03b0318520690a729c8ed5607908a7
mccarrion <matthewccarrion@gmail.com>
2018-11-07 11:54:55 -0700

    Updating axios with initial request for articles

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -2,14 +2,19 @@ import axios from 'axios';
 
 // This was written out in order to visualize how axios works as a starting
 // point on how to refactor and expand this file going forward
-axios.get('http://localhost:8000/api')
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+// axios.get('http://localhost:8000/api')
+//   .then(function (response) {
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
 
-function ArticleList() {
-  return axios.get('/articles');
+
+const getArticles = async () => {
+  try {
+    return await axios.get('http://localhost:8000/api/:subject/articles')
+  } catch (e) {
+    console.error(e)
+  }
 }
