59a5bb5049d7534c65d44e376feeff54b29bd26b
mccarrion <matthewccarrion@gmail.com>
2018-11-09 19:05:56 -0700

    Put in initial constructor on registration page

--- frontend/src/components/User/SignUp.js
+++ frontend/src/components/User/SignUp.js
@@ -1,12 +1,22 @@
 
 
 class SignUp extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+    this.submitForm = () => e => {
+      e.preventDefault();
+      this.props.onSubmit();
+    };
+  }
 
   render() {
     return (
       <div className="SignUp">
         <form onSubmit={this.handleSubmit}>
-          
+
         </form>
       </div>
     )
