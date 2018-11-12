c20b1563cc3590ab12c4dc63868f409638d1ed11
mccarrion <matthewccarrion@gmail.com>
2018-11-12 14:40:54 -0700

    Setting up Article page and ported over HTML from original templates

---
+++ frontend/src/components/Article/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Article extends React.Component {
+  render() {
+    return (
+      <div className="display-article">
+        <div className="container">
+          <div className="col-md-8 offset-md-2">
+            <h1>{this.props.article.title}</h1>
+            <img className="img-fluid" src="{this.props.image.url}" />
+            <h3>{this.props.article.subtitle}</h3>
+            <b>By {this.props.article.author}</b>
+            <p>{this.props.article.date}</p>
+            <p>{this.props.article.body|safe|linebreaks}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
