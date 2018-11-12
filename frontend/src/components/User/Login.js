12bc3361165be8608a10f0f0e9e38874f650b0d8
mccarrion <matthewccarrion@gmail.com>
2018-11-12 10:20:04 -0700

    Set the values that will be passed into the SignUp and Login forms

--- frontend/src/components/User/Login.js
+++ frontend/src/components/User/Login.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-
 class Login extends React.Component{
-  constructor() {
-    super();
-    this.submitForm = (email, password) => e => {
-      e.preventDefault();
-      this.props.onSubmit(email, password);
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      submitted: False
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      email: event.target.email,
+      password: event.target.password
+    });
+  }
+
+  handleSubmit(event) {
+    this.setState({ submitted: True });
+    event.preventDefault();
   }
 
   render() {
--- frontend/src/components/User/SignUp.js
+++ frontend/src/components/User/SignUp.js
@@ -1,15 +1,31 @@
-
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 class SignUp extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
+      fullName: '',
+      email: '',
+      password: '',
+      submitted: False
+    };
 
-    };
-    this.submitForm = () => e => {
-      e.preventDefault();
-      this.props.onSubmit();
-    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      fullName: event.target.fullName,
+      email: event.target.email,
+      password: event.target.password
+    });
+  }
+
+  handleSubmit(event) {
+    this.setState({ submitted: True });
+    event.preventDefault();
   }
 
   render() {
