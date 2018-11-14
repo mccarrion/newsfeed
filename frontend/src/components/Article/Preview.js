e579deba2d3f1940502e695e389f7d0d57409517
mccarrion <matthewccarrion@gmail.com>
2018-11-14 10:06:53 -0700

    Article preview now has broad outlines of how it should look

--- frontend/src/components/Article/Preview.js
+++ frontend/src/components/Article/Preview.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Preview = props => {
   const article = props.article;
 
   return (
     <div className="preview">
-      <Link to={`/:subject/${article.slug}`} className="link">
-        <h2>{article.title}</h2>
+      <Link to={`/${article.subject}/${article.slug}`} className="link">
+        <img src="{article.thumbnail.url}" />
       </Link>
+      <Link to={`/${article.subject}/${article.slug}`} className="link">
+        <h3>{article.title}</h3>
+      </Link>
+      <h5>{article.subtitle}</h5>
+      <p>By {article.author} on {article.date}</p>
     </div>
   );
 }
+
+export const Preview;
