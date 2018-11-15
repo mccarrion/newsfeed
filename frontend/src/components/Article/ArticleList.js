01006148fd4aa6c3f2babad68bbbfcc13d145a26
mccarrion <matthewccarrion@gmail.com>
2018-11-15 15:58:16 -0700

    Integrating load more functionality into ArticleList

--- frontend/src/components/Article/ArticleList.js
+++ frontend/src/components/Article/ArticleList.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
 class ArticleList extends React.Component {
-  constructor() {
-    super()
-    this.state = { articles: [] }
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles: [],
+      list: 10,
+      error: false
+    };
+
+    this.loadMore = this.loadMore.bind(this);
+  }
+
+  loadMore() {
+    this.setState((prev) => {
+      return {list: prev.list + 10};
+    })
   }
 
   componentDidMount() {
