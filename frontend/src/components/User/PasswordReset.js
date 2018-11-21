54a5435ac34b1e97d5f672675a2a97f1cda7a127
mccarrion <matthewccarrion@gmail.com>
2018-11-20 20:56:19 -0700

    Added constructor to password reset form

--- frontend/src/components/User/PasswordReset.js
+++ frontend/src/components/User/PasswordReset.js
@@ -1,6 +1,14 @@
 import React from 'react'
 
 class PasswordReset extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      password: '',
+      passwordAgain: '',
+      submitted: False
+    };
+
 
   render() {
     return (
