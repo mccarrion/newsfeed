552c40f36f98a989ef41bb660a5c0452dfbd78ad
mccarrion <matthewccarrion@gmail.com>
2018-12-04 19:28:51 -0700

    List of tabs for user profile and some cleanup

--- frontend/src/components/User/Profile.js
+++ frontend/src/components/User/Profile.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-class Profile extends React.Component {
+class Profile extends Component {
   componentWillMount() {
 
+  }
+
+  renderTabs() {
+    return (
+      <ul className="nav">
+        <li className="nav-item">
+          <Link
+            className="nav-link active"
+            to={`TODO`}>
+            Profile
+          </Link>
+        </li>
+
+        <li className="nav-item">
+          <Link
+            className="nav-link active"
+            to={`TODO`}>
+            Favorites
+          </Link>
+        </li>
+
+        <li className="nav-item">
+          <Link
+            className="nav-link active"
+            to={`TODO`}>
+            Comments
+          </Link>
+        </li>
+      </ul>
+    );
   }
 
   render() {
@@ -15,3 +45,5 @@ class Profile extends React.Component {
     );
   }
 }
+
+export default Profile;
