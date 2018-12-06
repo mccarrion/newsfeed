d7c2dc58da6e1880fb713214ccbce2403f63d37d
mccarrion <matthewccarrion@gmail.com>
2018-12-05 17:27:07 -0700

    Updated fields on PasswordChange and added submit handler

--- frontend/src/components/User/PasswordChange.js
+++ frontend/src/components/User/PasswordChange.js
@@ -4,9 +4,27 @@ class PasswordChange extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      password: '',
+      existingPassword: '',
+      newPassword: '',
+      newPasswordConfirm: '',
       submitted: False
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      existingPassword: event.target.existingPassword,
+      newPassword: event.target.newPassword,
+      newPasswordConfirm: event.target.newPasswordConfirm
+    });
+  }
+
+  handleSubmit(event) {
+    this.setState({ submitted: True });
+    event.preventDefault();
   }
 
   render() {
@@ -15,7 +33,7 @@ class PasswordChange extends React.Component {
         <form onSubmit={this.handleSubmit}>
           <Field type="password" name="existingPassword" required="required" />
           <Field type="password" name="newPassword" required="required" />
-          <Field type="password" name="newPasswordCheck" required="required" />
+          <Field type="password" name="newPasswordConfirm" required="required" />
           <button type="submit" name="action">Change Password</button>
         </form>
       </div>
