c613fdc78510e860d183d28283d33ccd6429238d
mccarrion <matthewccarrion@gmail.com>
2018-12-13 19:12:01 -0700

    Imports for existing components added to index

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 import { Router, Route } from 'react-router-dom';
+import Article from './Article/Article';
+import ArticleList from './Article/ArticleList';
+import Login from './User/Login';
+import Profile from './User/Profile';
+import SignUp from './User/SignUp';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <Route exact path="/" component={Home} />
+          {/* <Route exact path="/" component={Home} /> */}
           <Route path="/login"  component={Login} />
           <Route path="/signup" component={SignUp} />
           <Router exact path="/@:username" component={Profile} />
-          <Router path="/@:username/comments" component={Comments} />
-          <Router path="/@:username/favorites" component={Favorites} />
-          <Route exact path="/:subject" component={Subject} />
+          {/* <Router path="/@:username/comments" component={Comments} /> */}
+          {/* <Router path="/@:username/favorites" component={Favorites} /> */}
+          <Route exact path="/:subject" component={ArticleList} />
           <Route path="/:subject/:title" component={Article} />
         </header>
       </div>
--- frontend/src/components/User/Login.js
+++ frontend/src/components/User/Login.js
@@ -52,3 +52,5 @@ class Login extends Component{
     )
   }
 }
+
+export default Login;
