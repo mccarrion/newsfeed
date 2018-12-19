e00eee7340f520ca2e32e2e38460c8353e040680
Matthew Carrion <matthewccarrion@gmail.com>
2018-12-18 23:07:48 -0700

    Initial Home page component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Article from './Article/Article';
 import ArticleList from './Article/ArticleList';
 import Login from './User/Login';
@@ -11,14 +11,16 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          {/* <Route exact path="/" component={Home} /> */}
-          <Route path="/login"  component={Login} />
-          <Route path="/signup" component={SignUp} />
-          <BrowserRouter exact path="/@:username" component={Profile} />
-          {/* <Router path="/@:username/comments" component={Comments} /> */}
-          {/* <Router path="/@:username/favorites" component={Favorites} /> */}
-          <Route exact path="/:subject" component={ArticleList} />
-          <Route path="/:subject/:title" component={Article} />
+          <Switch>
+            {/* <Route exact path="/" component={Home} /> */}
+            <Route path="/login"  component={Login} />
+            <Route path="/signup" component={SignUp} />
+            <Route exact path="/@:username" component={Profile} />
+            {/* <Router path="/@:username/comments" component={Comments} /> */}
+            {/* <Router path="/@:username/favorites" component={Favorites} /> */}
+            <Route exact path="/:subject" component={ArticleList} />
+            <Route path="/:subject/:title" component={Article} />
+          </Switch>
         </header>
       </div>
     );
