500f7337695f7d03196598dc8bf86c628763af07
Matthew Carrion <matthewccarrion@gmail.com>
2018-12-28 02:45:39 -0700

    Getting Home page ready for API call and feed

--- frontend/src/components/common/Home.js
+++ frontend/src/components/common/Home.js
@@ -1,9 +1,19 @@
+import ArticleList from '../article/ArticleList';
 import React, { Component } from 'react';
 
 class Home extends Component {
+  componentWillMount() {
+
+  }
+
+  componentWillUnmount() {
+    this.props.onUnload();
+  }
+
   render() {
     return (
       <div className="home">
+        <ArticleList />
       </div>
     );
   }
