d54ef7a8f8aba5f210cd1e1cf8e89a7380226217
mccarrion <matthewccarrion@gmail.com>
2019-02-08 19:17:20 -0700

    Checking for header for token

--- newsfeed/users/tokens.py
+++ newsfeed/users/tokens.py
@@ -7,4 +7,14 @@ from .models import User
 class JWTAuthentication(BaseAuthentication):
     def authenticate(self, request):
         auth_header = get_authorization_header(request).split()
+        
+        if not auth_header:
+            return None
+        
+        if len(auth_header) == 1:
+            return None
+        
+        elif len(auth_header) > 2:
+            return None
+        
         return token
