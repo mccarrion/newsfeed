7583e8f93a0de01c0d518148216f09bce3a202c8
mccarrion <matthewccarrion@gmail.com>
2019-04-05 16:21:02 -0700

    HitCount for articles now shows up in JSON

--- newsfeed/articles/serializers.py
+++ newsfeed/articles/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 
 from .models import Article, Comment, Favorite
+from hitcount.models import HitCount
 from newsfeed.users.models import User
+
 
 
 class AuthorField(serializers.RelatedField):
@@ -13,14 +15,27 @@ class AuthorField(serializers.RelatedField):
 		return User.objects.get(id=id)
 
 
+class HitCountField(serializers.RelatedField):
+	def to_representation(self, obj):
+		data = obj.hits
+		return data
+
+	def to_internal_value(self, id):
+		return HitCount.objects.get(id=id)
+
+
 class ArticleSerializer(serializers.ModelSerializer):
     author = AuthorField(
     	queryset=User.objects.all(), many=True, required=True
     )
+    hit_count = HitCountField(
+    	queryset=HitCount.objects.all()
+    )
     class Meta:
         model = Article
-        fields = ('title', 'subtitle', 'thumbnail', 'author',
-                  'image', 'body', 'date', 'slug', 'whats_news','subject')
+        fields = ('title', 'subtitle', 'thumbnail', 'author', 
+                  'hit_count', 'image', 'body', 'date', 
+                  'slug', 'whats_news','subject')
 
 
 class CommentSerializer(serializers.ModelSerializer):
--- newsfeed/settings/wsgi.py
+++ newsfeed/settings/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "newsfeed.settings.heroku")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "newsfeed.settings.local")
 
 application = get_wsgi_application()
