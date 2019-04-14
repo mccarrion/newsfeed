d4331b155ab0939d4cdc27a0c4d8126b74992208
mccarrion <matthewccarrion@gmail.com>
2019-04-13 21:34:48 -0700

    JWT url paths added

--- newsfeed/users/urls.py
+++ newsfeed/users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+from rest_framework_jwt import views
 
 from .auth import RegisterView
 from .views import ProfileView
@@ -9,4 +10,6 @@ app_name = "users"
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='registration'),
     path('<username>/', ProfileView.as_view(), name='user_profile'),
+    path('login/', views.ObjtainJSONWebToken.as_view(), name='login'),
+    path('login/refresh/', views.RefreshJSONWebToken.as_view(), name='login_refresh'),
 ]
