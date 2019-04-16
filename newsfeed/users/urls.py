10adb1e860fe629ef52f098885a781c3941cdb70
mccarrion <matthewccarrion@gmail.com>
2019-04-15 19:33:28 -0700

    Updated urls for getting JWT token

--- newsfeed/users/urls.py
+++ newsfeed/users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from rest_framework_jwt import views
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
 from .auth import RegisterView
 from .views import ProfileView
@@ -10,6 +10,6 @@ app_name = "users"
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='registration'),
     path('<username>/', ProfileView.as_view(), name='user_profile'),
-    path('login/', views.ObjtainJSONWebToken.as_view(), name='login'),
-    path('login/refresh/', views.RefreshJSONWebToken.as_view(), name='login_refresh'),
+    path('api-token-auth/', obtain_jwt_token, name='login'),
+    path('api-token-refresh/', refresh_jwt_token, name='login_refresh'),
 ]
