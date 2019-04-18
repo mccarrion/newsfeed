29dddec81cb0be48d8fa7dde57d313e61996c112
mccarrion <matthewccarrion@gmail.com>
2019-04-17 19:42:09 -0700

    Removed all custom auth stuff because djoser

--- newsfeed/users/serializers.py
+++ newsfeed/users/serializers.py
@@ -3,39 +3,6 @@ from rest_framework import serializers
 from .models import User
 
 
-class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(
-        min_length=8, 
-        max_length=128, 
-        write_only=True
-    )
-    token = serializers.CharField(read_only=True)
-    
-    def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email']
-        )
-        # Password is not serialized because it is hashed
-        user.set_password(validated_data['password'])
-        user.save()
-        return user
-    
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'password', 'token')
-
-
-class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    email = serializers.EmailField()
-    password = serializers.CharField(max_length=128, write_only=True)
-    token = serializers.CharField(read_only=True)
-
-    def validate(self, data):
-        pass
-
-
 class UserSerializer(serializers.ModelSerializer):
     """
     This is the serializer for the User data.
--- newsfeed/users/urls.py
+++ newsfeed/users/urls.py
@@ -1,12 +1,10 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .auth import RegisterView
 from .views import ProfileView
 
 
 app_name = "users"
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='registration'),
     path('<username>/', ProfileView.as_view(), name='user_profile'),
 ]
