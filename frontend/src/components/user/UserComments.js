c4e08f4a825a5428ceb4024ebfb13d17fba985d5
mccarrion <matthewccarrion@gmail.com>
2019-04-19 21:30:40 -0700

    Initial UserComments component for user profiles

---
+++ frontend/src/components/user/UserComments.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { API_URL } from '../../constants/appConstants';
+
+class UserComments extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      comments: [],
+    };
+
+  }
+
+  componentDidMount() {
+    // TODO: Need to make this filter by user who is logged in
+    return axios.get(`${API_URL}/comments/`)
+      .then(res => {
+        this.setState({ comments: res.data });
+      })
+      .catch(error => {
+        console.log('Error while fetching!', error);
+      });
+  }
+
+  render() {
+    const { comments } = this.state;
+
+    return(
+      <div className="container">
+        {
+          comments.map((comment, index) =>
+            <div className="col-md-8">
+              <div key={index}>
+                <div className="row">
+                  <p>{comment.text}</p>
+                </div>
+              </div>
+            </div>
+          )
+        }
+      </div>
+    )
+  }
+}
+
+export default UserComments;
