b7478928fd54dd394185d4ab973601996f00dc9e
mccarrion <matthewccarrion@gmail.com>
2019-04-24 07:29:46 -0700

    Draft of comment form

--- frontend/src/components/article/CommentForm.js
+++ frontend/src/components/article/CommentForm.js
@@ -3,10 +3,36 @@ import axios from 'axios';
 import { API_URL } from '../../constants/appConstants';
 
 class CommentForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            text: ''
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const value = event.target.value;
+    const name = event.target.name;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    axios.post(`${API_URL}/comments/`, {
+      text: this.state.text
+    })
+  }
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
 }
+
+export default CommentForm;
