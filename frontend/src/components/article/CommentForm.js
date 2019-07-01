0f6188bb52ca451e7343a88aed2a0c52bffd9139
mccarrion <mccarrion@users.noreply.github.com>
2019-06-30 21:05:05 -0700

    Working version of CommentForm with CKEditor

--- frontend/src/components/article/CommentForm.js
+++ frontend/src/components/article/CommentForm.js
@@ -1,32 +1,29 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { API_URL } from '../../constants/appConstants';
-import CKEditor from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import CKEditor from 'ckeditor4-react';
 
 class CommentForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      comment: ''
+      data: '<p>Share your ideas</p>'
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event, editor) {
-    const value = editor.getData();
-    const name = event.target.name;
+  handleChange(event) {
     this.setState({
-      [name]: value
+      data: event.editor.getData()
     });
   }
 
   handleSubmit(event) {
     event.preventDefault();
     axios.post(`${API_URL}/comments/`, {
-      comment: this.state.comment
+      comment: this.state.data
     })
   }
 
@@ -34,20 +31,10 @@ class CommentForm extends Component {
     return (
       <div className="container">
         <h4>Comments</h4>
+        {/* TODO: Change to quilljs -> https://github.com/quilljs/quill */}
         <CKEditor 
-          editor={ ClassicEditor }
-          data="<p>Share your ideas</p>"
-          onInit={ editor => {
-            console.log('Editor is working!', editor);
-          }}
-          name="comment"
-          type="text"
-          value={this.state.comment}
-          onChange={this.handleChange}
-          // onChange={ (event, editor) => {
-          //   const data = editor.getData();
-          //   console.log({ event, editor, data });
-          // }}
+          data={this.state.data}
+          onChange={this.handleChange} 
         />
       </div>
     );
