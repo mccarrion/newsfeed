1bf0081ba37d64f2340da34b07a8f3d76b7494d9
mccarrion <mccarrion@users.noreply.github.com>
2019-08-12 21:29:17 -0700

    Updated authReducer slightly

--- frontend/src/reducers/authReducer.js
+++ frontend/src/reducers/authReducer.js
@@ -2,16 +2,12 @@ import * as types from '../actions/actionTypes';
 import initialState from './initialState';
 import { browserHistory } from 'react-router';
 
-const isEmpty = require("is-empty");
 
 export default function authReducer(state = initialState.auth, action) {
   switch(action.type) {
     case types.SIGNIN:
-      return {
-        ...state,
-        isAuthenticated: !isEmpty(action.payload),
-        user: action.payload
-      };
+      browserHistory.push('/')
+      return !!localStorage.getItem('id_token')
     default:
       return state;
   }
