24bb540327a1eae8461199368d5ab7d79ff97a3f
mccarrion <mccarrion@users.noreply.github.com>
2019-08-21 20:34:32 -0700

    Added subject reducer

---
+++ frontend/src/reducers/subjectReducer.js
@@ -0,0 +1,6 @@
+import * as types from '../actions/actionTypes';
+import initialState from './initialState';
+
+export default function subjectReducer(state = initialState.subjects, action) {
+    return state;
+}
