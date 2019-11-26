79dd38c3bf3f4ac9b512a2ac644b720ab30893b7
mccarrion <mccarrion@users.noreply.github.com>
2019-11-25 22:09:04 -0700

    Update to SUBJECT action name

--- frontend/src/actions/actionTypes.js
+++ frontend/src/actions/actionTypes.js
@@ -2,7 +2,7 @@
  * Action types are ordered alphabetically
  */
 export const LOGOUT = 'LOGOUT';
-export const SET_SUBJECT = 'SUBJECT';
+export const CHANGE_SUBJECT = 'CHANGE_SUBJECT';
 export const SIGNIN_SUCCESS = 'SIGNIN_SUCCESS';
 export const SIGNOUT_SUCCESS = 'SIGNOUT_SUCCESS';
 export const SIGNUP = 'SIGNUP';
--- frontend/src/reducers/subjectReducer.js
+++ frontend/src/reducers/subjectReducer.js
@@ -2,5 +2,10 @@ import * as types from '../actions/actionTypes';
 import initialState from './initialState';
 
 export default function subjectReducer(state = initialState.subjects, action) {
-    return state;
+    switch(action.type) {
+        case types.CHANGE_SUBJECT:
+            return action.subject
+        default:
+            return state;
+    }
 }
