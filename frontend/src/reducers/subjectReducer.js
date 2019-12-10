39088ca16079913a3db57285a1b6ff9e150ac3c7
mccarrion <mccarrion@users.noreply.github.com>
2019-12-09 20:40:02 -0700

    Updating subject reducer

--- frontend/src/reducers/subjectReducer.js
+++ frontend/src/reducers/subjectReducer.js
@@ -1,9 +1,11 @@
 import * as types from '../actions/actionTypes';
 import initialState from './initialState';
+import { browserHistory } from 'react-router';
 
 export default function subjectReducer(state = initialState.subject, action) {
     switch(action.type) {
-        case types.CHANGE_SUBJECT:
+        case types.SET_SUBJECT:
+            browserHistory.push(`/${action.subject}`)
             return action.subject
         default:
             return state;
