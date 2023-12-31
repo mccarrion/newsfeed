d5b34c20f3e3ff786834544e136617c6d0d3ab32
mccarrion <25594552+mccarrion@users.noreply.github.com>
2023-12-30 22:25:28 -0700

    can query for an article

--- backend/api/articles.py
+++ backend/api/articles.py
@@ -1,19 +1,27 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
-from ..schema import article as models
-from ..crud import crud_article as service
-from backend.db.config import engine, get_db
+from ..schema import article as schemas
+from ..crud import crud_article as crud
+from backend.db.config import get_db
 
 router = APIRouter()
 
 
 @router.post("/articles/create/")
-def create_article(article: models.ArticleCreate, db: Session = Depends(get_db)):
-    return service.create_article(db=db, article=article)
+def create_article(article: schemas.ArticleCreate, db: Session = Depends(get_db)):
+    return crud.create_article(db=db, article=article)
 
 
-@router.get("/articles/", response_model=list[models.Article])
-def get_articles(db: Session = Depends(get_db)):
-    articles = service.get_articles(db)
+@router.get("/articles/", response_model=list[schemas.Article])
+def get_articles(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    articles = crud.get_articles(db, skip=skip, limit=limit)
     return articles
+
+
+@router.get("/articles/{article_id}/", response_model=schemas.Article)
+def get_article(article_id: int, db: Session = Depends(get_db)):
+    db_article = crud.get_article(db, article_id=article_id)
+    if db_article is None:
+        raise HTTPException(status_code=404, detail="Article not found")
+    return db_article
--- backend/crud/crud_article.py
+++ backend/crud/crud_article.py
@@ -1,14 +1,18 @@
 from sqlalchemy.orm import Session
-from backend.schema import article as models
-from backend.db import db_article as dbschema
+from backend.schema import article as schema
+from backend.db import db_article as model
 
 
-def get_articles(db: Session):
-    return db.query(dbschema.Article).all()
+def get_articles(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(model.Article).offset(skip).limit(limit).all()
 
 
-def create_article(db: Session, article: models.ArticleCreate):
-    db_article = dbschema.Article(title=article.title, body=article.body, creator_id=article.creator_id)
+def get_article(db: Session, article_id: int):
+    return db.query(model.Article).filter(model.Article.id == article_id).first()
+
+
+def create_article(db: Session, article: schema.ArticleCreate):
+    db_article = model.Article(title=article.title, body=article.body, creator_id=article.creator_id)
     db.add(db_article)
     db.commit()
     db.refresh(db_article)
